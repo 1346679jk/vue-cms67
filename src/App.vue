08e439c96db0106d75efc1219aa63e8b402a88b0
jiangkai <1346679jk@163.com>
2019-05-06 20:33:54 +0800

    项目header和tabbar已完成

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- 头部 -->
+  <div id="app" class="app-container">
+    <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
+    <!-- 中间 -->
+    <router-view/>
+    <!-- 底部 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
+        <span class="mui-icon mui-icon-contact">
+          <span class="mui-badge">9</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.app-container {
+  padding-top: 40px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomeContainer from '../components/tabbar/HomeContainer.vue'
+import MemberContainer from '../components/tabbar/MemberContainer.vue'
+import SearchContainer from '../components/tabbar/SearchContainer.vue'
+import ShopcarContainer from '../components/tabbar/ShopcarContainer.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  linkActiveClass: 'mui-active',
+  routes: [
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: SearchContainer },
+    { path: '/search', component: ShopcarContainer }
+  ]
+})
